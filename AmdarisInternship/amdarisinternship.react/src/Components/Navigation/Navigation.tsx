ef41b35a5cde5fefb6b704a2b9c2e6d005f5cead
Eugen <eugenciolacu@gmail.com>
2020-11-19 20:19:26 +0200

    19.11.2020 1st commit

--- AmdarisInternship/amdarisinternship.react/src/Components/Navigation/Navigation.tsx
+++ AmdarisInternship/amdarisinternship.react/src/Components/Navigation/Navigation.tsx
@@ -3,6 +3,20 @@ import { makeStyles, Theme } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import { Paper } from '@material-ui/core';
+// import {
+//     BrowserRouter as Router,
+//     Switch,
+//     Route,
+//     Link
+// } from 'react-router-dom';
+import ModuleForm from '../ModuleForm/ModuleForm';
+
+// const routes = [
+//     {
+//         path: "/modules",
+//         component: ModuleForm
+//     }
+// ];
 
 export default function Navigation() {
     const classes = useStyles();
@@ -10,23 +24,39 @@ export default function Navigation() {
 
     const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
         setValue(newValue);
+
+        if (newValue == 0)
+        {
+            <ModuleForm />
+        }
     };
 
+    let tmp : any = <ModuleForm />;
+
+    if (value != 0)
+    {
+        tmp = null;
+    }
+
     return (
-        <Paper className={classes.root}>
-            <Tabs
-                value={value}
-                onChange={handleChange}
-                indicatorColor="primary"
-                textColor="primary"
-                centered
-            >
-                <Tab label="Modules" />
-                <Tab label="Promotions" />
-                <Tab label="Lessons" />
-                <Tab label="Users" />
-            </Tabs>
-        </Paper>
+        <>
+            <Paper className={classes.root}>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    centered
+                >
+                    <Tab label="Modules" />
+                    <Tab label="Promotions" />
+                    <Tab label="Lessons" />
+                    <Tab label="Users" />
+                </Tabs>
+            </Paper>
+
+            {tmp}
+        </>
     );
 }
 
