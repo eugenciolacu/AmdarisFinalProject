8dca7d51e7b063c5dc8f0576ddd01f7a4590166c
Eugen <eugenciolacu@gmail.com>
2020-11-21 18:22:30 +0200

    21.11.2020 2nd commit

--- AmdarisInternship/amdarisinternship.react/src/Components/HooksExample/HooksExample.tsx
+++ AmdarisInternship/amdarisinternship.react/src/Components/HooksExample/HooksExample.tsx
@@ -1,10 +1,64 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useRef } from 'react';
+import { useState } from 'react';
 
-export default function HooksExample ()
-{
+export default function HooksExample() {
     return (
         <>
-            Hooks
+            <>
+                <UseStateHookExample />
+                <UseEffectHookExample />
+                <UseRefHookExample />
+            </>
         </>
+    );
+}
+
+function UseStateHookExample() {
+    // Объявляем новую переменную состояния "count"
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <p>Вы кликнули {count} раз</p>
+            <button onClick={() => setCount(count + 1)}>
+                Нажми на меня
+      </button>
+        </div>
+    );
+}
+
+function UseEffectHookExample() {
+    const [count, setCount] = useState(0);
+
+    // По принципу componentDidMount и componentDidUpdate:
+    useEffect(() => {
+        // Обновляем заголовок документа, используя API браузера
+        document.title = `Вы нажали ${count} раз`;
+    });
+
+    return (
+        <div>
+            <p>Вы нажали {count} раз</p>
+            <button onClick={() => setCount(count + 1)}>
+                Нажми на меня
+        </button>
+        </div>
+    );
+}
+
+function UseRefHookExample () {
+    const inputRef = useRef <HTMLInputElement>(null);
+
+    const onButtonClick = () => {
+        inputRef.current?.focus();
+    };
+
+    return (
+        <div>
+            <input ref={inputRef} type="text"></input>
+            <button onClick={onButtonClick}>Focus the input</button>
+        </div>
     );
 }
